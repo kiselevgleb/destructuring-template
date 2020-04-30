70226e557f68ff2d1353ea7083ab36f804c8ee17
kiselevgleb <kiselevgleb@gmail.com>
2020-04-30 17:58:21 +0300

    transformed mas

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,26 @@
 // TODO: write your code here
 import sum from './basic';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function specialDetal(params) {
+  const {
+    special
+  } = params;
+  let mas = [];
+  special.forEach(element => {
+    const {
+      id,
+      name,
+      icon,
+      description = 'Описание недоступно'
+    } = element;
+    mas.push({
+      id,
+      name,
+      icon,
+      description
+    });
+  });
+  console.log(special);
+  console.log(mas);
+  return mas;
+}
